50a70c4032a57b070202a99b0c05d8b2c7f3c34e
Pokrotecki <148866837+Pokrotecki@users.noreply.github.com>
2026-04-13 16:49:22 +0200

    Add files via upload

---
+++ Projekt/Logowanie.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    
+        private void BTNZatwierdz_Clicked(object sender, RoutedEventArgs e)
+        {
+            //informacja bedzie wychodzila po sprawdzeniu danych uzytkownika wiec te zmienne tutaj sa zeby sie nie czepialo o bledy
+            bool czyistnieje = true;
+            int typkonta = 1;
+            Window okno = null;
+            if (czyistnieje == true)
+            {
+                if (typkonta == 1)
+                {
+                    okno = new PacjentOkno();
+                }
+                if(typkonta == 2)
+                {
+                    okno = new LekarzOkno();
+                }
+                if (typkonta == 3)
+                {
+                    okno = new PracownikOkno();
+                }
+                if (okno != null)
+                {
+                    okno.Show();
+                    this.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Projekt/PracownikOkno.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projekt
+{
+    /// <summary>
+    /// Logika interakcji dla klasy PracownikOkno.xaml
+    /// </summary>
+    public partial class PracownikOkno : Window
+    {
+        public PracownikOkno()
+        {
+            InitializeComponent();
+        }
+
+        private void BTNMagazyn_Clicked(object sender, RoutedEventArgs e)
+        {
+            Window okno = new MagazynOkno();
+            okno.Show();
+        }
+    }
+}
